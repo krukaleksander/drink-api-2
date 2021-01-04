fe215c03a137d686995f6b5e21c39f3b5d4ce51c
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-04 19:56:11 +0100

    install react-router-dom, set paths

--- src/App.js
+++ src/App.js
@@ -1,5 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import SingleCoctail from './pages/SingleCocktail';
+import Error from './pages/Error';
 
-const App = () => <h1>Drink API</h1>
+import Navbar from './components/Navbar';
+
+const App = () => {
+    return (
+        <Router>
+            <Navbar />
+            <Switch>
+                <Route exact path='/'>
+                    <Home />
+                </Route>
+                <Route path='/about'>
+                    <About />
+                </Route>
+                <Route path='/coctail/:id'>
+                    <SingleCoctail />
+                </Route>
+                <Route path='*'>
+                    <Error />
+                </Route>
+            </Switch>
+        </Router>
+
+    )
+}
 
 export default App;

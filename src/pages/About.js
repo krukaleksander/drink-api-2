2759800abb0b27ba735a62c7b202341ee05c00ab
Alek Kruk <kruk.aleksander@hotmail.com>
2021-01-05 17:30:31 +0100

    finish about and error page

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
+    <section className='section about-section'>
+      <h1 className='section-title'>about us</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores possimus asperiores necessitatibus consectetur similique facere soluta excepturi eum officia nostrum, quasi, distinctio doloribus assumenda aut, enim quae saepe voluptatibus repudiandae veniam error autem cumque deserunt. Provident, quas. Doloremque, ab qui?</p>
+    </section>
   )
 }
 
